0a1e78b55b842b758141bc92b1a11ed3b52d15ea
kagif101 <otnielsre@gmail.com>
2019-01-09 22:37:56 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Script Bot Aplikasi
+// @Author   : Ferry Kirdan Agustin
+// @Email    : npazone@gmail.com
+// @WhatsApp : +62895375136311
+// @Telegram : @ipkzone ( Iddant )
+
+$email = "otnielsre@gmail.com";
+$devid = "868594040672050";
+
+?>
